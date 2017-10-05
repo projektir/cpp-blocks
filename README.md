5a06f79ae3f5362ae94843a18492345d5d2da816
projektir <oprojektir@gmail.com>
2017-10-04 19:05:59 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# cpp-blocks
+
+To build via VC++:
+
+`cmake .. -G "Visual Studio 15 2017 Win64"`
+
+`MSBuild cpp-blocks.sln`
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Blar" << std::endl;
+    std::cout << "wfewfewfe" << std::endl;
+
+    return 0;
+}
