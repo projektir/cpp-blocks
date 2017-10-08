aaa57fa40a0a80c9efca5af5954e498c9b1ee272
projektir <oprojektir@gmail.com>
2017-10-07 18:09:22 -0700

    Moving code out of main

---
+++ src/game.hpp
@@ -0,0 +1,16 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include <iostream>
+#include "SDL.h"
+#include "SDL_image.h"
+
+const int SQUARE_SIZE = 40;
+
+enum Direction {UP, DOWN, LEFT, RIGHT};
+
+int init();
+int move_rect(SDL_Renderer *renderer, SDL_Texture* texture,
+    SDL_Rect *source_rect, SDL_Rect *dest_rect, Direction direction);
+
+#endif /* GAME_HPP */
