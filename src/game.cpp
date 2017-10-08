46ac0c11429d3aedd4f9414a630c940729347468
projektir <oprojektir@gmail.com>
2017-10-07 22:58:16 -0700

    Break up some functions

--- src/game.cpp
+++ src/game.cpp
@@ -1,11 +1,19 @@
 #include "game.hpp"
 
+bool process_event(SDL_Event* event,
+    SDL_Renderer *renderer, SDL_Texture* texture,
+    SDL_Rect *source_rect, SDL_Rect *dest_rect);
+
+void process_key(SDL_Keycode keycode,
+    SDL_Renderer *renderer, SDL_Texture* texture,
+    SDL_Rect *source_rect, SDL_Rect *dest_rect);
+
 int init() {
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window *window = SDL_CreateWindow("cpp-blocks",
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-        640, 480,
+        640, 880,
         0
     );
 
@@ -48,43 +56,13 @@ int init() {
     SDL_RenderCopy(renderer, texture, &source_rect, &dest_rect);
     SDL_RenderPresent(renderer);
 
-
-
     bool quit = false;
     SDL_Event event;
-    SDL_Keycode keycode;
 
     while (!quit) {
         while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_KEYDOWN:
-                    keycode = event.key.keysym.sym;
-                    switch(keycode) {
-                        case SDLK_a:
-                            move_rect(renderer, texture, &source_rect, &dest_rect,
-                                Direction::LEFT);
-                            break;
-                        case SDLK_d:
-                            move_rect(renderer, texture, &source_rect, &dest_rect,
-                                Direction::RIGHT);
-                            break;
-                        case SDLK_s:
-                            move_rect(renderer, texture, &source_rect, &dest_rect,
-                                Direction::DOWN);
-                            break;
-                        case SDLK_w:
-                            move_rect(renderer, texture, &source_rect, &dest_rect,
-                                Direction::UP);
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case SDL_QUIT:
-                    quit = true;
-                    break;
-                default:
-                    break;
+            if (process_event(&event, renderer, texture, &source_rect, &dest_rect)) {
+                quit = true;
             }
         }
     }
@@ -96,6 +74,51 @@ int init() {
     SDL_Quit();
 
     return 0;
+}
+
+bool process_event(SDL_Event* event,
+    SDL_Renderer *renderer, SDL_Texture* texture,
+    SDL_Rect *source_rect, SDL_Rect *dest_rect) {
+
+    switch (event->type) {
+        case SDL_KEYDOWN:
+            process_key(event->key.keysym.sym, renderer, texture,
+                source_rect, dest_rect);
+            break;
+        case SDL_QUIT:
+            return true;
+            break;
+        default:
+            break;
+    }
+
+    return false;
+}
+
+void process_key(SDL_Keycode keycode,
+    SDL_Renderer *renderer, SDL_Texture* texture,
+    SDL_Rect *source_rect, SDL_Rect *dest_rect) {
+    
+    switch (keycode) {
+        case SDLK_a:
+            move_rect(renderer, texture, source_rect, dest_rect,
+                Direction::LEFT);
+            break;
+        case SDLK_d:
+            move_rect(renderer, texture, source_rect, dest_rect,
+                Direction::RIGHT);
+            break;
+        case SDLK_s:
+            move_rect(renderer, texture, source_rect, dest_rect,
+                Direction::DOWN);
+            break;
+        case SDLK_w:
+            move_rect(renderer, texture, source_rect, dest_rect,
+                Direction::UP);
+            break;
+        default:
+            break;
+    }
 }
 
 // Convert this to a move-figure later
